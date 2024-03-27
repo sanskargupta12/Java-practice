3dcef97a721dff7f51f07bccb983e6480cfb7b35
Sanskar Gupta <121814254+sanskargupta12@users.noreply.github.com>
2024-03-27 12:23:59 +0530

    Update interface.java

--- interface.java
+++ interface.java
@@ -1,32 +1,101 @@
-// Java program to demonstrate working of
-// interface
+// Java program to demonstrate the 
+// real-world example of Interfaces
 
 import java.io.*;
 
-// A simple interface
-interface In1 {
-
-	// public, static and final
-	final int a = 10;
-
-	// public and abstract
-	void display();
+interface Vehicle {
+	
+	// all are the abstract methods.
+	void changeGear(int a);
+	void speedUp(int a);
+	void applyBrakes(int a);
 }
 
-// A class that implements the interface.
-class TestClass implements In1 {
-
-	// Implementing the capabilities of
-	// interface.
-	public void display(){ 
-	System.out.println("Geek"); 
+class Bicycle implements Vehicle{
+	
+	int speed;
+	int gear;
+	
+	// to change gear
+	@Override
+	public void changeGear(int newGear){
+		
+		gear = newGear;
 	}
+	
+	// to increase speed
+	@Override
+	public void speedUp(int increment){
+		
+		speed = speed + increment;
+	}
+	
+	// to decrease speed
+	@Override
+	public void applyBrakes(int decrement){
+		
+		speed = speed - decrement;
+	}
+	
+	public void printStates() {
+		System.out.println("speed: " + speed
+			+ " gear: " + gear);
+	}
+}
 
-	// Driver Code
-	public static void main(String[] args)
-	{
-		TestClass t = new TestClass();
-		t.display();
-		System.out.println(a);
+class Bike implements Vehicle {
+	
+	int speed;
+	int gear;
+	
+	// to change gear
+	@Override
+	public void changeGear(int newGear){
+		
+		gear = newGear;
+	}
+	
+	// to increase speed
+	@Override
+	public void speedUp(int increment){
+		
+		speed = speed + increment;
+	}
+	
+	// to decrease speed
+	@Override
+	public void applyBrakes(int decrement){
+		
+		speed = speed - decrement;
+	}
+	
+	public void printStates() {
+		System.out.println("speed: " + speed
+			+ " gear: " + gear);
+	}
+	
+}
+class GFG {
+	
+	public static void main (String[] args) {
+	
+		// creating an instance of Bicycle
+		// doing some operations
+		Bicycle bicycle = new Bicycle();
+		bicycle.changeGear(2);
+		bicycle.speedUp(3);
+		bicycle.applyBrakes(1);
+		
+		System.out.println("Bicycle present state :");
+		bicycle.printStates();
+		
+		// creating instance of the bike.
+		Bike bike = new Bike();
+		bike.changeGear(1);
+		bike.speedUp(4);
+		bike.applyBrakes(3);
+		
+		System.out.println("Bike present state :");
+		bike.printStates();
 	}
 }
