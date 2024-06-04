bcb12e4334621c58448ec7453c3df733d383b2de
Sanskar Gupta <121814254+sanskargupta12@users.noreply.github.com>
2024-06-04 12:44:31 +0530

    Update Primitive.java

--- Primitive.java
+++ Primitive.java
@@ -2,7 +2,7 @@ public class Primitive{
   public static void main(String[] args){
      int roll =64;
      char letter='r';
-     float marks= 98.67f;
+     float marks= 88.67f;
      double largedeci= 83838933893.8889;
      long largeinteger= 73892137894199810019L;
      boolean check= false;
