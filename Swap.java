d4e2df4b464e0f28c63768c59ce6636568e73d81
Sanskar Gupta <121814254+sanskargupta12@users.noreply.github.com>
2024-06-09 15:33:05 +0530

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -3,7 +3,7 @@ class Swap {
     public static void main(String[] args) {  
        int x, y, t;// x and y are to swap   
        Scanner sc = new Scanner(System.in);  
-       System.out.println("Enter the value of X and Y");  
+       System.out.println("Enter the value of X & Y");  
        x = sc.nextInt();  
        y = sc.nextInt();  
        System.out.println("before swapping numbers: "+x +"  "+ y);  
