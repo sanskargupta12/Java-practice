a44a53200cf0a357d68c2371ad1ffb75d87f52c4
Sanskar Gupta <121814254+sanskargupta12@users.noreply.github.com>
2024-06-17 17:33:40 +0530

    Update ArrayColNoFixed.java

--- ArrayColNoFixed.java
+++ ArrayColNoFixed.java
@@ -2,7 +2,7 @@ public class ArrayColNoFixed {
     public static void main(String[] args) {
         int[][] arr = {
                 {1, 2, 3, 4},
-                {5, 6},
+                {5, 7},
                 {7, 8, 9}
         };
 
