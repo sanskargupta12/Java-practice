53ddc620b25ac11bcebce908b1bd51a583150661
Sanskar Gupta <121814254+sanskargupta12@users.noreply.github.com>
2024-06-21 16:43:49 +0530

    Update Array_intro.java

--- Array_intro.java
+++ Array_intro.java
@@ -17,7 +17,7 @@ public class Array_intro {
 //        // or directly
 //        int[] rnos2 = {23, 12, 45, 32, 15};
 
-        int[] ros; // declaration of array. ros is getting defined in stack
+        int[] ros; // declaration of array. ros is getti
         ros = new int[5]; 
     System.out.println(ros[1]);
 
