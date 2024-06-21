2310bb3f05d7e3f6e844ab7ff1a89c4805ccf362
Sanskar Gupta <121814254+sanskargupta12@users.noreply.github.com>
2024-06-21 16:44:16 +0530

    Update Array_intro.java

--- Array_intro.java
+++ Array_intro.java
@@ -17,7 +17,7 @@ public class Array_intro {
 //        // or directly
 //        int[] rnos2 = {23, 12, 45, 32, 15};
 
-        int[] ros; // declaration of array
+        int[] ros; // d
         ros = new int[5]; 
     System.out.println(ros[1]);
 
