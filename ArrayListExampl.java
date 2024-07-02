5cb8bcc93d5b7b8f1979e585343621393f36c1d7
Sanskar Gupta <121814254+sanskargupta12@users.noreply.github.com>
2024-07-02 13:50:29 +0530

    Update ArrayListExampl.java

--- ArrayListExampl.java
+++ ArrayListExampl.java
@@ -27,7 +27,7 @@ public class ArrayListExample {
             list.add(in.nextInt());
         }
 
-        // get item at any 
+        // get item at
         for (int i = 0; i < 5; i++) {
             System.out.println(list.get(i)); 
         }
